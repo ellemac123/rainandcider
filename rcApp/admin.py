cd8593af41ceec77cc3acdfb44ffbe781f1800f2
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-18 15:49:28 -0400

    ad again

--- rcApp/admin.py
+++ rcApp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import City
 
-@admin.register(City)
+
 class CityAdmin(admin.ModelAdmin):
     search_fields = ('city',)
     list_display = ('city', 'country', 'location_id',)
