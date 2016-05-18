8fb28d0806779a433a33ef6e74b137e51a36d3fa
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-18 15:46:54 -0400

    admin.pu

--- rcApp/admin.py
+++ rcApp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
 from .models import City
 
-admin.site.register(City)
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ('city',)
+    list_display = ('city', 'country', 'location_id',)
+
+admin.site.register(City, CityAdmin)
