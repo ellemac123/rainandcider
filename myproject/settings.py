7d4364ed05243bf05bd4601e68a47262bf3b16fb
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-18 13:27:04 -0400

    a

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,6 +52,12 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'myproject.urls'
 
+
+TEMPLATE_DIRS = (
+     os.path.join(BASE_DIR, 'rcApp', 'templates'),
+)
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -119,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
