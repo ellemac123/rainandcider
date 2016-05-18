7c296afc851e0df63930aa81ef5d2c6fa436d884
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-18 14:51:55 -0400

    here; ;

--- rcApp/views.py
+++ rcApp/views.py
@@ -1,13 +1,23 @@
+from django.shortcuts import redirect, render,
 from django.shortcuts import render_to_response
-from django.shortcuts import redirect, render
+
+from .forms import CityForm
+from .models import *
 
 
 def home(request):
-     if request.method == 'POST':
-          return redirect('country:detail')
+    if request.method == 'POST':
+        cityform = CityForm(request.POST)
+        if cityform.is_valid():
+            citydata = cityform.cleaned_data
+            cityInfo = City.objects.get(id=citydata['city'])
 
-     return render_to_response('home/home.html')
+            # messages.success(request, 'Successfully Changed')
+        return redirect('country:detail', country_code=cityInfo.country, city_code=citydata['city'])
+
+    return render_to_response('home/home.html')
 
 
-def detail(request):
-     return render(request, 'country/detail.html')
+def detail(request, country_code, city_code):
+    data = {'country': country_code, 'city': city_code, }
+    return render(request, 'country/detail.html', data)
