d01571e2eac126e235f9e323404d6514b6116f37
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-19 11:58:30 -0400

    trying URLS for country

--- rcApp/urls.py
+++ rcApp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='index'),
-    #url(r'detail/(?P<country_code>[A-Z]{2})/$', views.detail, name = 'detail'),
-    #url(r'detail/(?P<country_code>[A-Z]{2})/(?P<city_code>([0-9]{1}|[0-9]{2}))/$', views.detail, name = 'detail'),
+    url(r'detail/(?P<country_code>[A-Z]{2})/$', views.detail, name = 'detail'),
+    url(r'detail/(?P<country_code>[A-Z]{2})/(?P<city_code>([0-9]{1}|[0-9]{2}))/$', views.detail, name = 'detail'),
 
     # url(r'detail/(?P<country_code>[A-Z]{2})/(?P<city_code>[0-9]{1})/$', views.detail, name = 'detail'),
     ]
