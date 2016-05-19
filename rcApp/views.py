0b26a8a5f2d83c58ed4980f4060fa368c59869e0
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-19 13:57:12 -0400

    trying urllib2

--- rcApp/views.py
+++ rcApp/views.py
@@ -48,7 +48,8 @@ def getNews(cityState, countryName):
         url = 'http://api.nytimes.com/svc/semantic/v2/concept/name/nytd_geo/' + name + '.json?fields=all&api-key=694311ac0a739a6c388fcebe9605c7d9:11:75176215'
         print(url)
         list = []
-        f = urllib.request.urlopen(url)
+        f = urllib2.urlopen(url)
+        #f = urllib.request.urlopen(url)
         content = f.read()
         decoded_response = content.decode('utf-8')
         jsonResponse = json.loads(decoded_response)
