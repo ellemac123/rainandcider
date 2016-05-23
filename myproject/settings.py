51e144737f19af9cd522c772503c83d791b48e30
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-23 12:02:01 -0400

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -95,16 +95,16 @@ else:
     LOG_DIR = '.'
 
 
-CACHE_PORT = '11211'
-# Production Environment
-if ON_OPENSHIFT:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-            'LOCATION': '%s:%s' % (os.environ['OPENSHIFT_PYTHON_IP'], CACHE_PORT),
-        }
-    }
-
+# CACHE_PORT = '11211'
+# #Production Environment
+# if ON_OPENSHIFT:
+#     CACHES = {
+#         'default': {
+#             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#             'LOCATION': '%s:%s' % (os.environ['OPENSHIFT_PYTHON_IP'], CACHE_PORT),
+#         }
+#     }
+#
 # # Development Environment
 # else:
 #     CACHES = {
