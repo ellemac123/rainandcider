96097f22c5f483bc1ffa6df62d593cb33738cca1
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-26 13:51:04 -0400

    celery.py with settings now

--- myproject/celery.py
+++ myproject/celery.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
-
+import os
 from django.conf import settings
 from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+
 
 app = Celery('myproject')
 app.config_from_object('django.conf:settings')
