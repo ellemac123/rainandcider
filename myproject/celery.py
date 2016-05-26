9cf6f02e2eb0ad3f50de5db1b765b31df175e987
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-26 11:40:45 -0400

    added __init__ and celery.py

--- myproject/celery.py
+++ myproject/celery.py
@@ -1,13 +1,13 @@
-# import os
-# from celery import Celery
-# from django.conf import settings
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-#
-# app = Celery('myproject')
-# app.config_from_object('django.conf:settings')
-# app.autodiscover_tasks(lambda : settings.INSTALLED_APPS)
-#
-# @app.task(bind=True)
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+app = Celery('myproject')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda : settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
