8e5e7f82ae868b7b1f0a0624e1912014c1650947
Laura Macaluso <lmacaluso@redlattice.com>
2016-05-31 20:18:28 -0400

    more

--- myproject/settings.py
+++ myproject/settings.py
@@ -102,11 +102,10 @@ if ON_OPENSHIFT:
                                                 'celerybeat_schedule')
     CELERYBEAT_SCHEDULE_PIDFILE = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', ''),
                                            'celerybeat.pid')
-    REDIS_URL = "redis://127.0.0.1:6379"
 
-# REDIS_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
-    #                                         os.environ.get('OPENSHIFT_REDIS_HOST', ''),
-     #                                        os.environ.get('OPENSHIFT_REDIS_PORT', ''))
+    REDIS_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
+                                            os.environ.get('OPENSHIFT_REDIS_HOST', ''),
+                                             os.environ.get('OPENSHIFT_REDIS_PORT', ''))
 else:
     LOG_DIR = '.'
     CELERYBEAT_SCHEDULE_FILENAME = 'celerybeat_schedule'
