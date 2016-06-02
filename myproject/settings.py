3f188e6bd6f84f153d333262613c9b6bae10fd59
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-02 12:49:17 -0400

    without the word redis://

--- myproject/settings.py
+++ myproject/settings.py
@@ -98,7 +98,7 @@ if ON_OPENSHIFT:
                                                 'celerybeat_schedule')
     CELERYBEAT_SCHEDULE_PIDFILE = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', ''),
                                            'celerybeat.pid')
-    REDIS_URL = "127.0.0.1:6379"
+    REDIS_URL = "127.0.0.1:16379"
 
 
     # REDIS_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
