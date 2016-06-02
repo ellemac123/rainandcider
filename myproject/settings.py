8994a79618da95bb58cdfa1336863b2eabac1377
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-02 14:06:29 -0400

    trying with the new config file

--- myproject/settings.py
+++ myproject/settings.py
@@ -98,12 +98,10 @@ if ON_OPENSHIFT:
                                                 'celerybeat_schedule')
     CELERYBEAT_SCHEDULE_PIDFILE = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', ''),
                                            'celerybeat.pid')
-    REDIS_URL = "127.0.0.1:16379"
-
-
-    # REDIS_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
-    #                                         os.environ.get('OPENSHIFT_REDIS_HOST', ''),
-    #                                          os.environ.get('OPENSHIFT_REDIS_PORT', ''))
+    # REDIS_URL = "127.0.0.1:16379"
+    REDIS_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
+                                            os.environ.get('OPENSHIFT_REDIS_HOST', ''),
+                                             os.environ.get('OPENSHIFT_REDIS_PORT', ''))
 else:
     LOG_DIR = '.'
     CELERYBEAT_SCHEDULE_FILENAME = 'celerybeat_schedule'
