1aaa9bc30be96372c56435bf0b2ab18b60db6b2d
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-06 15:00:33 -0400

    commit

--- rcApp/tests.py
+++ rcApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import City
 
-# Create your tests here.
+
+class createCityTestCase(TestCase):
+    def setUp(self):
+        City.objects.create(country='FR', city='Paris', location_id='FRXX0076', id=1)
+
+    def test_create_city(self):
+        paris = City.objects.get(city='Paris')
+        self.assertEqual(paris.cache_key('item'), 'item_FR_1')
