08db6447a4b718100bc2b30862edb7b6b828532e
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-06 10:26:48 -0400

    comments

--- rcApp/tasks.py
+++ rcApp/tasks.py
@@ -6,17 +6,19 @@ from .views import *
 BROKER_URL = "redis://:{}@{}:{}".format(os.environ.get('OPENSHIFT_REDIS_PASSWORD', ''),
                                         os.environ.get('OPENSHIFT_REDIS_HOST', ''),
                                         os.environ.get('OPENSHIFT_REDIS_PORT', ''))
-
-#app = Celery('tasks', broker=BROKER_URL, backend=BROKER_URL)
 app = Celery('tasks')
 logger = get_task_logger('raincider')
 
+"""
+@:param run_every   The task will run every 10 seconds
+@:param name        The name of the task is update_cache
+Update_cache will be called by the celery beat and the
+cache will call update_city for each city each time
+this is called. This is so the users will 'never' have
+to wait for the page to load.
+"""
 @app.task(run_every=10, name="update_cache")
 def update_cache():
-    """
-    This will update the cache so that the user
-    'never' has to wait for content to load
-    """
     logger.debug('Called the cache')
     for city in City.objects.all():
         update_city(city.country, city.pk)
