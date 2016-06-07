ffd857b59159066d89d66a14cf6f683c0454b9ab
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-07 14:32:24 -0400

    added the new cityModelForm to city Form

--- rcApp/forms.py
+++ rcApp/forms.py
@@ -8,7 +8,7 @@ Creates a city form that will allow you to
 select a city from the ones created by the
 admin. Used by the home view and html.
 """
-class CityForm(forms.ModelForm):
+class CityForm(forms.Form):
     city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label=None)
 
     # class Meta:
