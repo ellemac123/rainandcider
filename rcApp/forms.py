a88e3f016005013979f6b44a41235dd536dc0226
Laura Macaluso <lmacaluso@redlattice.com>
2016-06-08 11:01:44 -0400

    moving forms comments inside

--- rcApp/forms.py
+++ rcApp/forms.py
@@ -2,12 +2,11 @@ from django import forms
 
 from .models import City
 
-"""
-Creates a city form that will allow you to
-select a city from the ones created by the
-admin. Used by the home view and html.
-"""
-
 
 class CityForm(forms.Form):
+    """
+    Creates a city form that will allow you to
+    select a city from the ones created by the
+    admin. Used by the home view and html.
+    """
     city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label=None)
